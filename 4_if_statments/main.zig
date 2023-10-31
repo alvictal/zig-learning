37269a4dffe8c1a3264f1966293d01cfb886d894
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2023-10-31 10:02:19 -0300

    feat: Adding code about if statments and use of test/expect functions

---
+++ 4_if_statments/main.zig
@@ -0,0 +1,21 @@
+// Using test / expect argument to show how to test the code. You need to use "zig test main.zig" instead "zig run main.zig" to run the tests.
+const expect = @import("std").testing.expect;
+
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+
+    try expect(x == 1);
+}
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
