5e73aaee11b8fd31bdf61c58ad6746c8e1892729
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2023-10-31 08:54:38 -0300

    fix: Deleting old hello world directory

--- hello_world/main.zig
+++
@@ -1,8 +0,0 @@
-// Create a const variable and associate to a library, similiar with python "import numpy as np"
-const std = @import("std");
-
-// Create the void function main, make the function public
-pub fn main() void {
-    // Using the std library to use function print from class/struct debug with one string argument. Argumen used between {}
-    std.debug.print("Hello, {s}!\n", .{"World"});
-}
